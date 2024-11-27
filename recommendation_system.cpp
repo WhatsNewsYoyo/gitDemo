1eb6a5ab0545277877d2a9ea205be08b93a7c266
DaniloTato <Noentiendo2005@gmail.com>
2024-11-26 18:34:50 -0600

    add friend function

--- recommendation_system.cpp
+++ recommendation_system.cpp
@@ -7,7 +7,7 @@ struct user
 
 };
 
-int findIndex(const std::vector<user*>& vec, user* element) {
+int find_index(const std::vector<user*>& vec, user* element) {
     auto it = std::find(vec.begin(), vec.end(), element);
     if (it != vec.end()) {
         return std::distance(vec.begin(), it);
@@ -20,8 +20,8 @@ class RecommendationSystem{
 public:
     void addUser(std::string new_user);
     void addContent();
-    void addFriend();
-    void recommendContent();
+    void addFriend(std::string the_user, std::string his_friend);
+    std::vector<std::string> recommendContent(std::string user);
 private:
     std::vector<user*> _users;
     std::vector<std::string> _contents;
@@ -29,24 +29,40 @@ private:
     std::vector<std::vector<int>> _system;
 
     void update_system();
+    int find_by_name(std::string name);
 
 };
+
+int RecommendationSystem::recommendContent(std::string user) {
+    find_by_name[user];
+}
+
+int RecommendationSystem::find_by_name(std::string name) {
+    for (int i = 0; i < _users.size(); i++){
+        if(_users[i]->name == name) return i;
+    }
+    return -1;
+}
 
 void RecommendationSystem::addUser(std::string new_user){
     _users.push_back(new user);
     _users.back() -> name = new_user;
-    
 }
+
+void RecommendationSystem::addFriend(std::string the_user, std::string his_friend){
+    user* friend_ptr = _users[find_by_name(the_user)];
+    _users[find_by_name(the_user)]->friends.push_back(friend_ptr);
+}
+
 
 void RecommendationSystem::update_system(){
 
     int users_s = _users.size();
-
     std::vector<std::vector<int>> new_system(users_s, std::vector<int>(users_s));
 
     for(int i = 0; i < users_s; i++){
         for(auto friendd : _users[i] -> friends){
-            _system[i][findIndex(_users, friendd)];
+            _system[i][find_index(_users, friendd)];
         }
     }
 }
