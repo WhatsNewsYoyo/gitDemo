b7ffed20aff52054aaed19f966a64e85238f722f
WhatsNewsYoyo <garciayael715@gmail.com>
2024-11-26 18:20:22 -0600

    header 2

--- graph.h
+++ graph.h
@@ -1,24 +1,24 @@
-#ifndef GRAFO_H
-#define GRAFO_H
+#ifndef GRAPH_H
+#define GRAPH_H
 
 #include <iostream>
 #include <vector>
 #include <queue>
 #include <string>
+#include <map>
 
-#include <unordered_map>
 
 using namespace std;
 
 class Grafo {
 public:
     vector<vector<int>> Lista; 
-    unordered_map<string, int> MapaNombres;
+    map<string, int> MapaNombres;
     vector<string> Nombres; 
     void agregarNodo(const string& nombre);
     void agregarConexion(const string& a, const string& b); 
     vector<string> bfs(const string& inicio); 
-    Grafo();
+
 };
 
 #endif
